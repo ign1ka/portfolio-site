fc28623f1c762e44c5010e1b9c092667b0103769
Niles Dorn <nilesdorn@gmail.com>
2024-08-08 16:37:35 -0700

    Update homepage text

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,14 +4,14 @@
 
     <div class="paragraph">
       <div>
-        I'm <strong>Niles Dorn</strong>, a versatile software developer and hobbyist musician.<br/>
+        I'm <strong>Niles Dorn</strong>, a versatile library specialist and hobbyist musician.<br/>
         
-        I have strong skills across many disciplines, including object-oriented programming, the Unity and Unreal game engines, XR development, and audio production.
+        I work with antiquarian cartographic materials and unique visualization technologies at the David Rumsey Map Center. Along with my library expertise I've worked across many disciplines, picking up skills in object-oriented programming, the Unity and Unreal game engines, XR development, and audio production.
       </div>
 
-      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">development projects</router-link>, released <router-link to="/other-projects">independent music</router-link>, and applied my abilities in <router-link to="/resume">other fields</router-link> as well.</div>
+      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">development projects</router-link>, released <router-link to="/other-projects">independent music</router-link>, and delved into <router-link to="/resume">other fields</router-link> as well.</div>
 
-      <div style="margin-top: 40px;">I'm <strong>currently looking for full-time employment</strong>. You can reach me at <a href="mailto:nilesdorn@gmail.com">nilesdorn@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I'm <strong>currently looking for part-time employment</strong>. You can reach me at <a href="mailto:nilesdorn@gmail.com">nilesdorn@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
 
     <div class="photo">
